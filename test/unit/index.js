adc3bd3efa3e124e69d0ffc679e7560dfa1de44f
Todd Beauchamp <toddbee@gmail.com>
2017-01-03 11:22:34 -0500

    chore(build): update travis.yml

--- test/unit/index.js
+++ test/unit/index.js
@@ -8,8 +8,8 @@ Function.prototype.bind = require('function-bind')
 var testsContext = require.context('./specs', true, /\.spec$/)
 testsContext.keys().forEach(testsContext)
 
-// require all src files except main.js for coverage.
+// require all src files except index.js for coverage.
 // you can also change this to match only the subset of files that
 // you want coverage for.
-var srcContext = require.context('../../src', true, /^\.\/(?!main(\.js)?$)/)
+var srcContext = require.context('../../src', true, /^\.\/(?!index(\.js)?$)/)
 srcContext.keys().forEach(srcContext)
