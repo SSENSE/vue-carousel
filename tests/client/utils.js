b704379e63b7d6bbc794ca49aabb2180aa0faf47
Todd Beauchamp <toddbee@gmail.com>
2017-01-05 09:52:32 -0500

    fix(travis): attempt to fix tidylib issue

--- tests/client/utils.js
+++ tests/client/utils.js
@@ -1,13 +1,14 @@
 const Vue = require('vue');
-const tidy = require('htmltidy').tidy;
+const tidy = require('libtidy').compat.htmltidy.tidy;
 
 const options = {
   doctype: 'html5',
-  hideComments: true,
-  showBodyOnly: true,
+  'hide-comments': true,
+  'show-body-only': true,
   indent: true,
-  indentAttributes: true,
-  verticalSpace: true,
+  'indent-attributes': true,
+  'vertical-space': true,
+  'drop-empty-elements': false,
   wrap: 0,
   tabSize: 2
 };
@@ -17,10 +18,15 @@ function expectToMatchSnapshot(vm, element) {
     Vue.nextTick().then(() => {
       const html = (element || vm.$el).innerHTML;
       tidy(html, options, (err, cleanHtml) => {
+        if (err) {
+          return reject(err);
+        }
+
         expect(cleanHtml).toMatchSnapshot();
         resolve();
       });
-    }).catch((err) => reject(err));
+    }).catch(reject);
   });
 }
+
 exports.expectToMatchSnapshot = expectToMatchSnapshot;
