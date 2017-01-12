b566a5954599b986860487ba177b725eae43aa55
Todd Lawton Beauchamp <toddbee@gmail.com>
2017-01-12 10:30:43 -0500

    chore(lint): update rules to eslint-config-ssense (#7)

--- src/index.js
+++ src/index.js
@@ -1,14 +1,14 @@
-import Carousel from './Carousel.vue';
-import Slide from './Slide.vue';
+import Carousel from "./Carousel.vue"
+import Slide from "./Slide.vue"
 
 const install = (Vue) => {
-  Vue.component('carousel', Carousel);
-  Vue.component('slide', Slide);
-};
+  Vue.component("carousel", Carousel)
+  Vue.component("slide", Slide)
+}
 
-export default install;
+export default install
 
 export {
   Carousel,
   Slide,
-};
+}
--- src/utils/debounce.js
+++ src/utils/debounce.js
@@ -1,3 +1,4 @@
+/* eslint-disable fp/no-let */
 /*
  * Returns a function, that, as long as it continues to be invoked, will not
  * be triggered. The function will be called after it stops being called for
@@ -5,22 +6,22 @@
  * leading edge, instead of the trailing.
 */
 const debounce = (func, wait, immediate) => {
-  let timeout;
+  let timeout
   return () => {
-    const context = this;
+    const context = this
     const later = () => {
-      timeout = null;
+      timeout = null
       if (!immediate) {
-        func.apply(context);
+        func.apply(context)
       }
-    };
-    const callNow = immediate && !timeout;
-    clearTimeout(timeout);
-    timeout = setTimeout(later, wait);
-    if (callNow) {
-      func.apply(context);
     }
-  };
-};
+    const callNow = immediate && !timeout
+    clearTimeout(timeout)
+    timeout = setTimeout(later, wait)
+    if (callNow) {
+      func.apply(context)
+    }
+  }
+}
 
-export default debounce;
+export default debounce
