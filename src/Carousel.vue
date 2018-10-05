b739705b374c0f2500f354b09b2fd6b50f1ee143
Adam <adam-26@users.noreply.github.com>
2018-10-05 16:34:24 +0200

    fix(lint): Missing semi-colon
    Fix listing issue

--- src/Carousel.vue
+++ src/Carousel.vue
@@ -513,7 +513,7 @@ export default {
             childList: true,
             subtree: true,
             characterData: true
-          }
+          };
         }
         this.mutationObserver = new MutationObserver(() => {
           this.$nextTick(() => {
