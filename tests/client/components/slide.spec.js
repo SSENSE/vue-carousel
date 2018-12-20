de9a54a736e7e7fadc2e068530b2a08e9fae6793
Joseph Ting <josephting@users.noreply.github.com>
2018-12-21 06:45:37 +0900

    Add jest-serializer-vue as snapshot serializer (#341)

--- tests/client/components/slide.spec.js
+++ tests/client/components/slide.spec.js
@@ -1,11 +1,10 @@
 import { mount } from '@vue/test-utils';
-const utils = require('../utils');
 
 const Carousel = require('../../../src/Carousel.vue');
 const Slide = require('../../../src/Slide.vue');
 
 describe('Slide', () => {
-  it('should mount successfully', () => {
+  it('should mount successfully', done => {
     const wrapper = mount(Carousel, {
       slots: {
         default: [Slide]
@@ -13,6 +12,9 @@ describe('Slide', () => {
     });
     expect(wrapper.vm.$children[0]._isMounted).toBeTruthy();
 
-    return utils.expectToMatchSnapshot(wrapper.vm);
+    wrapper.vm.$nextTick(() => {
+      expect(wrapper).toMatchSnapshot();
+      done();
+    });
   });
 });
