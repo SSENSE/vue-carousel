91f63288a29459555f5baa35e603004119dcbf91
Filip Kudła <filip.kudla@snow.dog>
2019-07-14 18:27:57 +0200

    Update ssr webpack config to point to production webpack config

--- webpack.ssr.js
+++ webpack.ssr.js
@@ -1,9 +1,9 @@
 const merge = require('webpack-merge')
-const base = require('./webpack.common')
+const base = require('./webpack.prod.js')
 
 module.exports = merge(base, {
   target: 'node',
   output: {
-    filename: 'vue-carousel-ssr.js'
+    filename: 'vue-carousel-ssr.min.js'
   }
 })
