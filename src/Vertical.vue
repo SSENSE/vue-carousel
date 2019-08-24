b0daed45e4dd97cdb6c818f5c84e791d71fe8532
amirali <amiraliamhh@gmail.com>
2019-08-24 10:55:18 +0430

    feat(vertical): added vertical carousel

---
+++ src/Vertical.vue
@@ -0,0 +1,66 @@
+<template>
+    <div
+    id="VueCarousel-vertical-carousel-container"
+    >
+        <div
+        ref="VueCarouselContainer"
+        id="VueCarousel-vertical-carousel"
+        :style="{ maxHeight: `${maxHeight}px`, }"
+        >
+            <slot></slot>
+        </div>
+        <vertical-navigation 
+        @down="goDown"
+        @up="goUp"
+        >
+        <template slot="VueCarousel-nav-up" >
+            <slot name="VueCarousel-nav-up" />
+        </template>
+        <template slot="VueCarousel-nav-down" >
+            <slot name="VueCarousel-nav-down" />
+        </template>
+        </vertical-navigation>
+    </div>
+</template>
+
+<script>
+import VerticalNavigation from './VerticalNavigation';
+
+export default {
+    components: {
+        VerticalNavigation,
+    },
+    props: {
+        maxHeight: {
+            type: Number,
+            default: 500,
+        },
+    },
+    methods: {
+        goDown() {
+            this.$refs['VueCarouselContainer'].scrollTop += this.$refs['VueCarouselContainer'].firstElementChild.offsetHeight;
+        },
+        goUp() {
+            this.$refs['VueCarouselContainer'].scrollTop -= this.$refs['VueCarouselContainer'].firstElementChild.offsetHeight;
+        },
+    },
+};
+</script>
+
+<style>
+#VueCarousel-vertical-carousel-container {
+    width: 100%;
+    position: relative;
+    overflow: hidden;
+}
+
+#VueCarousel-vertical-carousel {
+    width: 100%;
+    position: relative;
+    overflow: hidden;
+}
+
+div {
+    scroll-behavior: smooth;
+}
+</style>
---
+++ src/VerticalNavigation.vue
@@ -0,0 +1,47 @@
+<template>
+    <div id="VueCarousel-vertical-navigation" >
+        <div
+        class="VueCarousel-vertical-button"
+        @click="goUp"
+        id="VueCarousel-nav-up">
+            <slot name="VueCarousel-nav-up" >
+                up
+            </slot>
+        </div>
+        <div 
+        class="VueCarousel-vertical-button"
+        @click="goDown"
+        id="VueCarousel-nav-down">
+            <slot name="VueCarousel-nav-down" >
+                down
+            </slot>
+        </div>
+    </div>    
+</template>
+
+<script>
+export default {
+    methods: {
+        goDown() {
+            this.$emit('down');
+        },
+        goUp() {
+            this.$emit('up');
+        },
+    },
+};
+</script>
+
+<style>
+.VueCarousel-vertical-button {
+    position: absolute;
+}
+
+#VueCarousel-nav-up {
+    top: 0;
+}
+
+#VueCarousel-nav-down {
+    bottom: 0;
+}
+</style>
