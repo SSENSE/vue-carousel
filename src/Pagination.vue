a0673d48a7752ca8672d99fd3de0e66ac8b44a34
Abbas Fathi <a.fathi@Jabama.ir>
2020-01-07 17:28:37 +0330

    feat: add instagram dot style to pagination

--- src/Pagination.vue
+++ src/Pagination.vue
@@ -2,9 +2,16 @@
   <div
     v-show="carousel.pageCount > 1"
     class="VueCarousel-pagination"
-    v-bind:class="{ [`VueCarousel-pagination--${paginationPositionModifierName}`]: paginationPositionModifierName }"
+    v-bind:class="{
+      [`VueCarousel-pagination--${paginationPositionModifierName}`]: paginationPositionModifierName
+    }"
   >
-    <div class="VueCarousel-dot-container" role="tablist" :style="dotContainerStyle">
+    <div
+      class="VueCarousel-dot-container"
+      role="tablist"
+      :style="dotContainerStyle"
+    >
+      <!-- Abbas -->
       <button
         v-for="(page, index) in paginationCount"
         :key="`${page}_${index}`"
@@ -15,7 +22,10 @@
         :value="getDotTitle(index)"
         :aria-label="getDotTitle(index)"
         :aria-selected="isCurrentDot(index) ? 'true' : 'false'"
-        v-bind:class="{ 'VueCarousel-dot--active': isCurrentDot(index) }"
+        v-bind:class="[
+          { 'VueCarousel-dot--active': isCurrentDot(index) },
+          dotClassList[index]
+        ]"
         v-on:click="goToPage(index)"
         :style="dotStyle(index)"
       ></button>
@@ -27,6 +37,12 @@
 export default {
   name: "pagination",
   inject: ["carousel"],
+  // Abbas
+  data() {
+    return {
+      dotClassList: []
+    };
+  },
   computed: {
     paginationPositionModifierName() {
       const { paginationPosition } = this.carousel;
@@ -50,17 +66,27 @@ export default {
         return {
           "margin-top": `${carousel.paginationPadding * 2}px`
         };
+
       const doublePadding = carousel.paginationPadding * 2;
+      // Abbas
       const containerWidth =
-        carousel.maxPaginationDotCount *
+        (carousel.maxPaginationDotCount + 4) *
+          (carousel.paginationSize + doublePadding) +
         (carousel.paginationSize + doublePadding);
+
       return {
         "margin-top": `${carousel.paginationPadding * 2}px`,
         overflow: "hidden",
         width: `${containerWidth}px`,
-        margin: "0 auto",
         "white-space": "nowrap"
       };
+    },
+    // Abbas
+    currentPage() {
+      return this.carousel.currentPage;
+    },
+    slideCount() {
+      return this.carousel.slideCount;
     }
   },
   methods: {
@@ -76,7 +102,6 @@ export default {
        */
       this.$emit("paginationclick", index);
     },
-
     /**
      * Check on current dot
      * @param {number} index - dot index
@@ -119,34 +144,101 @@ export default {
         }`
       });
 
-      if (carousel.maxPaginationDotCount === -1) return basicBtnStyle;
+      return basicBtnStyle;
 
-      const eachDotsWidth =
-        carousel.paginationSize + carousel.paginationPadding * 2;
-      const maxReverse = carousel.pageCount - carousel.maxPaginationDotCount;
-      const translateAmount =
-        carousel.currentPage > maxReverse
-          ? maxReverse
-          : carousel.currentPage <= carousel.maxPaginationDotCount / 2
-            ? 0
-            : carousel.currentPage -
-              Math.ceil(carousel.maxPaginationDotCount / 2) +
-              1;
-      const transformWidth = 0 - eachDotsWidth * translateAmount;
-      return Object.assign(basicBtnStyle, {
-        "-webkit-transform": `translate3d(${transformWidth}px,0,0)`,
-        transform: `translate3d(${transformWidth}px,0,0)`,
-        "-webkit-transition": `-webkit-transform ${carousel.speed / 1000}s`,
-        transition: `transform ${carousel.speed / 1000}s`
-      });
+      // Abbas
+
+      // const eachDotsWidth =
+      //   carousel.paginationSize + carousel.paginationPadding * 2;
+      // const maxReverse = carousel.pageCount - carousel.maxPaginationDotCount;
+      // const translateAmount =
+      //   carousel.currentPage > maxReverse
+      //     ? maxReverse
+      //     : carousel.currentPage <= carousel.maxPaginationDotCount / 2
+      //       ? 0
+      //       : carousel.currentPage -
+      //         Math.ceil(carousel.maxPaginationDotCount / 2) +
+      //         1;
+      // const transformWidth = 0 - eachDotsWidth * translateAmount;
+      // return Object.assign(basicBtnStyle, {
+      //   "-webkit-transform": `translate3d(${transformWidth}px,0,0)`,
+      //   transform: `translate3d(${transformWidth}px,0,0)`,
+      //   "-webkit-transition": `-webkit-transform ${carousel.speed / 1000}s`,
+      //   transition: `transform ${carousel.speed / 1000}s`
+      // });
+    },
+    setDotClassList(className, index) {
+      if (this.dotClassList[index] !== undefined) {
+        this.dotClassList[index] = className;
+      }
+    }
+  },
+  // Abbas
+  watch: {
+    currentPage(newPage, oldPage) {
+      if (
+        this.carousel.slideCount > this.carousel.maxPaginationDotCount &&
+        this.dotClassList[newPage].includes("small-scale")
+      ) {
+        if (newPage > oldPage) {
+          this.setDotClassList("", newPage);
+          this.setDotClassList("small-scale", newPage + 1);
+          this.setDotClassList("extra-small-scale", newPage + 2);
+          this.setDotClassList(
+            "small-scale",
+            newPage - this.carousel.maxPaginationDotCount
+          );
+          this.setDotClassList(
+            "extra-small-scale",
+            newPage - (this.carousel.maxPaginationDotCount + 1)
+          );
+          this.setDotClassList(
+            "zero-scale",
+            newPage - (this.carousel.maxPaginationDotCount + 2)
+          );
+        } else {
+          this.setDotClassList("", newPage);
+          this.setDotClassList(
+            "small-scale",
+            newPage + this.carousel.maxPaginationDotCount
+          );
+          this.setDotClassList("small-scale", newPage - 1);
+          this.setDotClassList("extra-small-scale", newPage - 2);
+          this.setDotClassList(
+            "extra-small-scale",
+            newPage + (this.carousel.maxPaginationDotCount + 1)
+          );
+          this.setDotClassList(
+            "zero-scale",
+            newPage + (this.carousel.maxPaginationDotCount + 2)
+          );
+        }
+      }
+    },
+    slideCount(count) {
+      if (this.carousel.maxPaginationDotCount !== -1) {
+        for (let i = 0; i < count; i++) {
+          if (i >= this.carousel.maxPaginationDotCount) {
+            if (i === this.carousel.maxPaginationDotCount) {
+              this.dotClassList[i] = "small-scale";
+            } else if (i === this.carousel.maxPaginationDotCount + 1) {
+              this.dotClassList[i] = "extra-small-scale";
+            } else {
+              this.dotClassList[i] = "zero-scale";
+            }
+          } else {
+            this.dotClassList[i] = "";
+          }
+        }
+      }
     }
   }
 };
 </script>
 
 <style scoped>
+/* Abbas */
 .VueCarousel-pagination {
-  width: 100%;
   text-align: center;
 }
 
@@ -176,9 +268,27 @@ export default {
   padding: 0;
   border-radius: 100%;
   outline: none;
+  transition: all 0.12s linear;
 }
 
 .VueCarousel-dot:focus {
   outline: 1px solid lightblue;
+}
+/* Abbas */
+
+.zero-scale {
+  transform: scale(0);
+  width: 0 !important;
+  height: 0 !important;
+  padding: 0 !important;
+}
+
+.small-scale {
+  transform: scale(0.5);
+}
+
+.extra-small-scale {
+  transform: scale(0.3);
+  pointer-events: none;
 }
 </style>
